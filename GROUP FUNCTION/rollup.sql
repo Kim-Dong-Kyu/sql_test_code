513d8ba1bccb31653c86f56fc29785b8029632b0
unknown <ekakth2458@gmail.com>
2020-03-05 10:31:10 +0900

    [UPDATE] 오타 수정

--- GROUP FUNCTION/rollup.sql
+++ GROUP FUNCTION/rollup.sql
@@ -1,4 +1,4 @@
-SELECT emp_no, SUM(salary) AS "43624의 매출 합계"
+SELECT emp_no, SUM(salary) AS "특정 emp의 매출 합계"
 FROM salaries 
 WHERE emp_no=43624 OR emp_no= 254466 OR emp_no =47978
 GROUP BY emp_no WITH ROLLUP
