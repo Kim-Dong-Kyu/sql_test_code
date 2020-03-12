57b84c14c9e2855000427d80716773cc1ee872c1
dkkim <ekakth2458@gmail.com>
2020-03-12 16:43:57 +0900

    [CREATE] INSERT QUERTY

---
+++ Hierarchical Query/Insert_Sample_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO "Hierarchical Query_Table" VALUES('철수','대리','순희');
+INSERT INTO "Hierarchical Query_Table" VALUES('순희','과장','연희');
+INSERT INTO "Hierarchical Query_Table" VALUES('길동','대리','순희');
+INSERT INTO "Hierarchical Query_Table" VALUES('영희','부장','개똥');
+INSERT INTO "Hierarchical Query_Table" VALUES('개똥','사장','없음');
