eba32358208f9dc230e428ab93832175d97bc628
dkkim <ekakth2458@gmail.com>
2020-03-20 17:42:19 +0900

    hackerrank 설명추가

--- HACKER_RANK/hacker_rank1.sql
+++ HACKER_RANK/hacker_rank1.sql
@@ -1,4 +1,5 @@
 --Query all columns for all American cities in CITY with populations larger than 100000. The CountryCode for America is USA.
+--테이블 구조가 주워지고 population이 1000000 보다 크고 countrycode가 usa인 칼럼 조회
 SELECT *
 FROM CITY
 WHERE POPULATION >=100000 AND COUNTRYCODE ='USA';
