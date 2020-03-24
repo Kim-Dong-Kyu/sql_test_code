0de33a4c37c5f8fb7ad718c4617b099fb085d0f5
dkkim <ekakth2458@gmail.com>
2020-03-24 09:22:41 +0900

    문자열 관련 쿼리

--- HACKER_RANK/hacker_rank2.sql
+++ HACKER_RANK/hacker_rank2.sql
@@ -28,3 +28,25 @@ FROM STATION
 WHERE LOWER(SUBSTR(CITY,1,1)) IN ('a', 'e', 'i', 'o', 'u')
  AND SUBSTR(CITY,-1) IN ('a', 'e', 'i', 'o', 'u')
 ORDER BY CITY ASC;
+
+--첫번 쨰 글자가 모음이 아닌 CITY이름의 목록을 출력
+SELECT DISTINCT CITY
+FROM STATION
+WHERE SUBSTR(CITY,1,1)NOT IN ('A', 'E', 'I','O','U');
+
+--마지막 글자가 모음이 아닌 CITY이름의 목록을 출력
+SELECT DISTINCT CITY
+FROM STATION
+WHERE UPPER(SUBSTR(CITY,-1)) NOT IN('A','E','O','I','U');
+
+-- 첫번 째 글자가 모음이 아니거나 마지막 글자 이름이 모음이 아닌 CITY 이름 목록 
+SELECT DISTINCT CITY
+FROM STATION
+WHERE (SUBSTR(CITY,1,1) NOT IN ('A','E', 'I','O','U')) 
+  OR (SUBSTR(CITY,-1) NOT IN ('a','e', 'i','o','u'));
+
+--첫번 째 글자가 모음이 이고 마지막 글자 이름이 모음이 아닌 CITY 이름 목록 
+SELECT DISTINCT CITY
+FROM STATION
+WHERE (SUBSTR(CITY,1,1) NOT IN ('A','E', 'I','O','U')) 
+  AND (SUBSTR(CITY,-1) NOT IN ('a','e', 'i','o','u'));
