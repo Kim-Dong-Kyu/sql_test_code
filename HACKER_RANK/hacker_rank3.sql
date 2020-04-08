46e58663323cbb98f7664e666f7664c03ef209ed
dkkim <ekakth2458@gmail.com>
2020-04-08 17:28:39 +0900

    ㅅㅂ 모르겠다 이건

--- HACKER_RANK/hacker_rank3.sql
+++ HACKER_RANK/hacker_rank3.sql
@@ -48,7 +48,7 @@ FROM OCCUPATIONS
 GROUP BY OCCUPATION
 ORDER BY COUNT(OCCUPATION), OCCUPATION;
 
---무슨 코드인지 모름 공부 필요 
+--무슨 코드인지 모름 공부 필요 ㅅㅂ 진짜 모르겠다~
 SET @r1=0, @r2=0, @r3 =0, @r4=0;
 SELECT MIN(Doctor), MIN(Professor), MIN(Singer), MIN(Actor) FROM
 (SELECT CASE Occupation WHEN 'Doctor' THEN @r1:=@r1+1
