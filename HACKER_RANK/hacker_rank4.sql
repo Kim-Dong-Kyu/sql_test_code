740dcb35df145162308135db45a5a93519cd6e2c
dkkim <ekakth2458@gmail.com>
2020-04-15 18:39:27 +0900

    upload

--- HACKER_RANK/hacker_rank4.sql
+++ HACKER_RANK/hacker_rank4.sql
@@ -8,7 +8,26 @@ SELECT SUM(POPULATION)
 FROM CITY
 WHERE DISTRICT ='California';
 
---캘리포니아 인구스의 평균
+--캘리포니아 인구수의 평균 
 SELECT AVG(POPULATIOn)
 FROM CITY
 WHERE DISTRICT ='California';
+
+--평균 반올림
+SELECT ROUND(AVG(POPULATION))
+FROM CITY;
+
+
+--일본의 인구수 합계 
+SELECT SUM(POPULATION)
+FROM CITY
+WHERE COUNTRYCODE  ='JPN';
+
+--최대 인구수와 최소 인구수의 차이
+SELECT MAX(POPULATION) - MIN(POPULATION)
+FROM CITY;
+
+--문제 해석이 너무 힘들다 0키가 고장나서 안썼다는 거 같은데 0인부분을 제거 하고 에러 오차를 구하는 함수 구현
+SELECT 
+CEIL(AVG(Salary)-AVG(REPLACE(Salary,'0','')))
+FROM EMPLOYEES;
