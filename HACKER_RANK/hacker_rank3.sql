68eecc738ce79d11bbec36bd4c0a4b7426017e22
dkkim <ekakth2458@gmail.com>
2020-04-18 23:19:02 +0900

    하나 남은건 언제 해석하지

--- HACKER_RANK/hacker_rank3.sql
+++ HACKER_RANK/hacker_rank3.sql
@@ -48,7 +48,7 @@ FROM OCCUPATIONS
 GROUP BY OCCUPATION
 ORDER BY COUNT(OCCUPATION), OCCUPATION;
 
---무슨 코드인지 모름 공부 필요 ㅅㅂ 진짜 모르겠다~
+--이건 언제 해석 하냐 ...
 SET @r1=0, @r2=0, @r3 =0, @r4=0;
 SELECT MIN(Doctor), MIN(Professor), MIN(Singer), MIN(Actor) FROM
 (SELECT CASE Occupation WHEN 'Doctor' THEN @r1:=@r1+1
