526622a3cfb5eae9662d5aa4d4cb398a298e4e74
dkkim <ekakth2458@gmail.com>
2020-05-13 08:50:31 +0900

    sequence

---
+++ sql 활용/sequence.sql
@@ -0,0 +1,13 @@
+
+/*
+  시퀀스 : pk 중복성을 방지하기 위해 주로 쓰이고 자동으로 insert 구분이 발생할때 index 값을 증가시켜 준다 
+*/
+CREATE SEQUENCE SEQ_TEST
+  START WITH 8000  --시작 번호
+  INCREMENT BY 1   --증가 량
+  CACHE 20;
+
+SELECT SEQ_TEST.NEXTVAL FROM DUAL;  
+-- INDEX 증가를 확인할 수 있다 .
+SELECT SEQ_TEST.CURRVAL FORM DUAL 
+--현재 인덱스를 확인할 수 있다.
