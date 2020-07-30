93cdce425d5fcf1b96a57527b47fccc7e6eca855
dkkim <ekakth2458@gmail.com>
2020-07-30 17:03:27 +0900

    [시간 단위 변환]

---
+++ sql 활용/시간변환.sql
@@ -0,0 +1,21 @@
+SELECT 
+  (  
+     SYSDATE 
+    - to_date('2020-07-29 10:00', 'yyyy-mm-dd hh24:mi')) *24 *60 *60 
+  AS min_diff
+FROM DUAL
+-- 초
+SELECT 
+  (  
+     SYSDATE 
+    - to_date('2020-07-29 10:00', 'yyyy-mm-dd hh24:mi')) *24 *60 
+  AS min_diff
+FROM DUAL
+-- 분 
+SELECT 
+  (  
+     SYSDATE 
+    - to_date('2020-07-29 10:00', 'yyyy-mm-dd hh24:mi')) *24  
+  AS min_diff
+FROM DUAL
+-- 시
