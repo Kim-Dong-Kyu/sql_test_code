7fe02ceafc4b98bd57f368e92e47e90b595c1892
dkkim <ekakth2458@gmail.com>
2020-07-30 17:04:38 +0900

    [UPDATE] 시간변환

--- sql 활용/시간변환.sql
+++ sql 활용/시간변환.sql
@@ -1,8 +1,8 @@
 SELECT 
   (  
-     SYSDATE 
-    - to_date('2020-07-29 10:00', 'yyyy-mm-dd hh24:mi')) *24 *60 *60 
-  AS min_diff
+    SYSDATE-
+     to_date('2020-07-29 10:00', 'yyyy-mm-dd hh24:mi')) *24 *60 *60 
+  AS min_di+ff
 FROM DUAL
 -- 초
 SELECT 
