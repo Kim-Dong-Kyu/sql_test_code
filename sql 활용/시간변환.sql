279f3d33f5209f3e5ddeff4a735c1d44799595c6
dkkim <ekakth2458@gmail.com>
2020-07-31 09:06:49 +0900

    [update] 일 시 분초 예제

--- sql 활용/시간변환.sql
+++ sql 활용/시간변환.sql
@@ -19,3 +19,16 @@ SELECT
   AS min_diff
 FROM DUAL
 -- 시
+
+SELECT EXTRACT( DAY    FROM difference ) AS days,
+       EXTRACT( HOUR   FROM difference ) AS hours,
+       EXTRACT( MINUTE FROM difference ) AS minutes,
+       EXTRACT( SECOND FROM difference ) AS seconds
+FROM   (
+  SELECT NUMTODSINTERVAL(
+           TO_DATE( '2016-01-02 01:01:12', 'YYYY-MM-DD HH24:MI:SS' )
+             - TO_DATE( '2016-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS' ),
+           'DAY'
+         ) AS difference
+  FROM   DUAL
+);// 일 시분 초 예제
