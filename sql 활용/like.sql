e76e71f07ae17d1051afa1e8721e4efb3df14012
dkkim <ekakth2458@gmail.com>
2021-05-04 10:42:06 +0900

    not like

--- sql 활용/like.sql
+++ sql 활용/like.sql
@@ -4,3 +4,5 @@ SELECT * FROM employees WHERE email LIKE '%com';
  -- com으로 끝나는 이메일을 조회
 SELECT * FROM employees WHERE first_name LIKE '%재%'
 -- '재' 글자 앞뒤로 어떤것이 와도 상관없는  이름을 조회
+SELECT * FROM employees WHERE first_name  NOT LIKE '%재%'
+-- '재' 라는 글자가 first_name 컬럼에 없는 경우를 모두 출력 
