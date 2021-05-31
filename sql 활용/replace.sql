b0119cc4911a985a5d94e01bb805e77487975d46
dkkim <ekakth2458@gmail.com>
2021-06-01 08:50:51 +0900

    replace 활용

--- sql 활용/replace.sql
+++ sql 활용/replace.sql
@@ -1,2 +1,18 @@
 --특정문자 치환
 replace(A.NM, 'Steven', 'S.')
+--특정문자 제거
+
+replace(A.NM, 'King', '')
+
+--엔터값 제거
+
+replace(replace(A.NM, chr(13), ''), chr(10), '')
+--탭문자 제거
+
+replace(A.NM, chr(9), '')
+--<br/>태그 엔터값으로 치환
+
+replace(A.NM, '<br/>', chr(13)||chr(10))
+
+--전환번호 구분자 제거
+replace(A.TELNO, '-', '')
